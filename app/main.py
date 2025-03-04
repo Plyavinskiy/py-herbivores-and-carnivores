9ead421591a387f3b0cc90a70d2871d0c3bf26d6
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2025-03-04 20:08:12 +0200

    implement py-herbivores-and-carnivores

--- app/main.py
+++ app/main.py
@@ -1,1 +1,48 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(
+        self,
+        name: str,
+        health: int = 100,
+        hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def receive_damage(self, damage: int) -> None:
+        self.health -= damage
+        if self.health <= 0:
+            self.die()
+
+    def die(self) -> None:
+        if self in Animal.alive:
+            Animal.alive.remove(self)
+
+    def __repr__(self) -> str:
+        return (
+            "{"
+            f"Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}"
+            "}"
+        )
+
+    @classmethod
+    def __str__(cls) -> str:
+        return "[" + ", ".join(repr(animal) for animal in cls.alive) + "]"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    @staticmethod
+    def bite(animal: Animal) -> None:
+        if not isinstance(animal, Herbivore) or animal.hidden:
+            return
+        animal.receive_damage(50)
